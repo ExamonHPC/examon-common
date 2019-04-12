4bc6edb4ae4c3155ad40c8e821f54888cff21f0a
Francesco Beneventi <francesco.beneventi@unibo.it>
2019-04-12 15:11:45 +0200

    Improved kairosdb client

--- examon/utils/kairosdb.py
+++ examon/utils/kairosdb.py
@@ -6,6 +6,7 @@ import requests
 import StringIO
 
 
+           
 class KairosDB:
     """
         KairosDB REST client
@@ -15,18 +16,21 @@ class KairosDB:
         self.port = port
         self.user = user
         self.password = password
-    
+        self.s = requests.Session()
+        self.s.auth = (self.user, self.password)
+        #self.s.headers.update({'x-test': 'true'})
+        self.apis = {}
+        self.api_server = "http://" + self.server + ":" + self.port
+        self.apis['post_metrics'] = self.api_server + "/api/v1/datapoints"
+        self.apis['post_query'] = self.api_server + "/api/v1/datapoints/query"
     
     def _compress(self, payload):
         s = StringIO.StringIO()
         with gzip.GzipFile(fileobj=s, mode='w') as g:
-            #g.write(json.dumps([x for x in http_payload_gen()]))
             g.write(json.dumps(payload))
         return s.getvalue()
     
-    
     def put_metrics(self, metrics, comp=True):
-        api = "http://" + self.server + ":" + self.port + "/api/v1/datapoints"
         headers = {}
         if comp:
             headers = {'content-type': 'application/gzip'}
@@ -34,12 +38,15 @@ class KairosDB:
         else:
             payload = metrics
         try:
-            if self.user is not None:
-                response = requests.post(api, payload, headers=headers, auth=(self.user, self.password))
-            else:
-                response = requests.post(api, payload, headers=headers)
+            response = self.s.post(self.apis['post_metrics'], payload, headers=headers)
+            #print response
         except:
             e = sys.exc_info()[0]
             #logger.error("[%s] Exception in post(): %s", "KairosDB", e)
             print "[%s] Exception in post(): %s" % ("KairosDB", e,)
-            pass
+            pass 
+    
+    def query_metrics(self, query):
+        headers = {'Accept-Encoding': 'gzip, deflate'}
+        response = self.s.post(self.apis['post_query'], data=json.dumps(query), headers=headers)
+        return response.json()
