b2102bef1b9591415082661d929d98a22ba5fc5c
Francesco Beneventi <francesco.beneventi@unibo.it>
2019-05-12 04:34:13 +0200

    Enabled module level logging

--- examon/db/kairosdb.py
+++ examon/db/kairosdb.py
@@ -5,7 +5,7 @@ import gzip
 import json
 import requests
 import StringIO
-
+import logging
 
            
 class KairosDB:
@@ -20,6 +20,7 @@ class KairosDB:
         self.s = requests.Session()
         self.s.auth = (self.user, self.password)
         #self.s.headers.update({'x-test': 'true'})
+        self.logger = logging.getLogger(__name__)
         self.apis = {}
         self.api_server = "http://" + self.server + ":" + self.port
         self.apis['post_metrics'] = self.api_server + "/api/v1/datapoints"
@@ -39,17 +40,20 @@ class KairosDB:
         else:
             payload = json.dumps(metrics)
         try:
+            self.logger.debug("Inserting %d metrics" % len(metrics))
             response = self.s.post(self.apis['post_metrics'], payload, headers=headers)
             response.raise_for_status()
         except:
             e = sys.exc_info()[0]
             #logger.error("[%s] Exception in post(): %s", "KairosDB", e)
-            print "[%s] Exception in post(): %s" % ("KairosDB", e,)
-            print "[%s] Reason: " % ("KairosDB",)
-            print response.text
+            self.logger.error("Exception in post()", exc_info=True)
+            #print "[%s] Exception in post(): %s" % ("KairosDB", e,)
+            #print "[%s] Reason: " % ("KairosDB",)
+            #print response.text
             pass 
     
     def query_metrics(self, query):
+        self.logger.debug("query metrics: %s" % repr(query))
         headers = {'Accept-Encoding': 'gzip, deflate'}
         response = self.s.post(self.apis['post_query'], data=json.dumps(query), headers=headers)
         return response.json()
